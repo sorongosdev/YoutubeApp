e4370c063e7bc64ee59be992422efc229a3de91d
Sorongos <dosora9@gmail.com>
2025-01-04 19:25:22 +0900

    feat: RecyclerView Adapter에 뷰홀더, DiffUtil 구현
    - ViewHolder 생성 및 바인딩 로직 구현
    - DiffUtil.ItemCallback을 사용해 아이템 비교 로직 추가

--- app/src/main/java/com/example/youtubeapp/VideoAdapter.kt
+++ app/src/main/java/com/example/youtubeapp/VideoAdapter.kt
@@ -1,6 +1,41 @@
 package com.example.youtubeapp
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.youtubeapp.databinding.ItemVideoBinding
 
-class VideoAdapter: ListAdapter<> {
+class VideoAdapter: ListAdapter<VideoItem, VideoAdapter.ViewHolder>(diffUtil) {
+    inner class ViewHolder(private val binding: ItemVideoBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(item: VideoItem){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemVideoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object{
+        val diffUtil = object : DiffUtil.ItemCallback<VideoItem>(){
+            override fun areItemsTheSame(oldItem: VideoItem, newItem: VideoItem): Boolean{
+                return oldItem.id == newItem.id
+            }
+            override fun areContentsTheSame(oldItem: VideoItem, newItem: VideoItem): Boolean{
+                return oldItem == newItem
+            }
+        }
+    }
 }
