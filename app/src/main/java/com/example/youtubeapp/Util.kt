cc16906ad93e50a36a8da510f7ab0f11582a2dc8
Sorongos <dosora9@gmail.com>
2025-01-04 23:33:05 +0900

    fix: JsonSyntaxException 해결
    - inputStream을 문자열로 캐스팅하는 방식에서 byteArray를 문자열로 캐스팅하는 것으로 오류 해결

--- app/src/main/java/com/example/youtubeapp/Util.kt
+++ app/src/main/java/com/example/youtubeapp/Util.kt
@@ -2,6 +2,7 @@ package com.example.youtubeapp
 
 import android.content.Context
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import java.io.IOException
 
 fun <T> Context.readData(fileName: String, classT: Class<T>): T? {
@@ -12,10 +13,13 @@ fun <T> Context.readData(fileName: String, classT: Class<T>): T? {
         inputStream.close() // 다 읽었으면 닫아줌
 
         val gson = Gson()
-        gson.fromJson(inputStream.toString(), classT) // inputStream을 문자열로 바꾸고, classT로 가져옴
+        gson.fromJson(String(byteArray), classT) // byteArray를 문자열로 바꾸고, classT로 가져옴
 
 
     } catch(e: IOException){
+        null
+    } catch(e: JsonSyntaxException){
+        e.printStackTrace()
         null
     }
 }
