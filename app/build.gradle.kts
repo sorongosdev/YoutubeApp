a233077bdc221612d384c8c0db30a2f472679ae4
Sorongos <dosora9@gmail.com>
2025-01-04 18:46:33 +0900

    chore: Gson 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.google.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
