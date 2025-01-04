6e13ae03ce4678889a126dc1493d29f027e805ec
Sorongos <dosora9@gmail.com>
2025-01-04 18:34:10 +0900

    feat: 메인 비디오 리스트 어댑터 연결

--- app/src/main/java/com/example/youtubeapp/MainActivity.kt
+++ app/src/main/java/com/example/youtubeapp/MainActivity.kt
@@ -2,14 +2,25 @@ package com.example.youtubeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.youtubeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var binding: ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var videoAdapter: VideoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        videoAdapter = VideoAdapter()
+
+        binding.videoListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = videoAdapter
+        }
     }
 }
