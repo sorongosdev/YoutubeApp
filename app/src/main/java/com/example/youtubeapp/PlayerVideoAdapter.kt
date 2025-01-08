d3cdbade0796ae4e51309a4c56c4d71008cc9774
Sorongos <dosora9@gmail.com>
2025-01-09 00:23:58 +0900

    feat: PlayerVideoAdapter 클래스 구현

--- app/src/main/java/com/example/youtubeapp/PlayerVideoAdapter.kt
+++ app/src/main/java/com/example/youtubeapp/PlayerVideoAdapter.kt
@@ -1,25 +1,87 @@
 package com.example.youtubeapp
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.youtubeapp.databinding.ItemVideoBinding
+import com.example.youtubeapp.databinding.ItemVideoHeaderBinding
 
-class PlayerVideoAdapter: ListAdapter<VideoItem,PlayerVideoAdapter.ViewHolder>(diffUtil) {
-    inner class ViewHolder(): RecyclerView.ViewHolder(){
+class PlayerVideoAdapter(private val context: Context,private val onClick: (VideoItem) -> Unit): ListAdapter<VideoItem, RecyclerView.ViewHolder>(diffUtil) {
+    inner class HeaderViewHolder(private val binding: ItemVideoHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: VideoItem) {
 
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    inner class VideoViewHolder(private val binding: ItemVideoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: VideoItem) {
+            binding.titleTextView.text = item.title
+            binding.subTitleTextView.text = context.getString(
+                R.string.sub_title_video_info,
+                item.channelName,
+                item.viewCount,
+                item.dateText
+            )
 
+            Glide.with(binding.videoThumbnailImageView)
+                .load(item.videoThumb)
+                .into(binding.videoThumbnailImageView)
+
+            Glide.with(binding.channelLogoImageView)
+                .load(item.channelThumb)
+                .circleCrop()
+                .into(binding.channelLogoImageView)
+
+            binding.root.setOnClickListener {
+                onClick.invoke(item)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_HEADER) {
+            HeaderViewHolder(
+                ItemVideoHeaderBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        } else {
+            VideoViewHolder(
+                ItemVideoBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
     }
 
-    companion object{
-        val diffUtil = object: DiffUtil.ItemCallback<VideoItem>(){
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == VIEW_TYPE_HEADER) {
+            //ViewHolder 안에 있는 bind 함수를 호출
+            (holder as HeaderViewHolder).bind(currentList[position])
+        } else {
+            (holder as VideoViewHolder).bind(currentList[position])
+        }
+    }
+
+    // 멀티 뷰타입 구현 가능
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) VIEW_TYPE_HEADER else VIEW_TYPE_VIDEO
+    }
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = 0
+        private const val VIEW_TYPE_VIDEO = 1
+        val diffUtil = object : DiffUtil.ItemCallback<VideoItem>() {
             override fun areItemsTheSame(oldItem: VideoItem, newItem: VideoItem): Boolean {
                 return oldItem.id == newItem.id
             }
